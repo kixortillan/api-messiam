1136df64b85ef647f3f7a2cb0e06b61fc3242634
Marieto Ortillan Jr <marieto.ortillan@gmail.com>
2018-07-11 16:43:19 +0800

    Initial commit

---
+++ functions/index.js
@@ -0,0 +1,20 @@
+const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+
+admin.initializeApp();
+
+// // Create and Deploy Your First Cloud Functions
+// // https://firebase.google.com/docs/functions/write-firebase-functions
+//
+// exports.helloWorld = functions.https.onRequest((request, response) => {
+//  response.send("Hello from Firebase!");
+// });
+
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  
+});
+
+exports.api = functions.https.onRequest(app);
